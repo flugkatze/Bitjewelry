d5ea1ff4f1a156c7166d3ad498a3fe76607f791c
赵宇翔 <zhaoyuxiang@zhaoyuxiangdeMacBook-Pro-3.local>
2019-05-20 13:30:53 +0800

    first commit

---
+++ Trezor/write_tool.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#st-flash write combined.bin 0x8000000
+
+arm-none-eabi-gdb --nx -ex 'set remotetimeout unlimited' -ex 'set confirm off' -ex 'target remote 127.0.0.1:3333' -ex 'monitor reset halt' -ex 'monitor flash write_image erase /home/jack/work/eclipse_mcu/bithd/spartacus/bithd-mcu/bootloader/combine/combined.bin 0x08000000' -ex 'monitor reset halt' /home/jack/work/eclipse_mcu/bithd/spartacus/bithd-mcu/bootloader/bootloader.elf
